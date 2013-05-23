1bee3512a5da5e98c6cf5cdbfcfcf3d704592504
psmits <thestinkypete@gmail.com>
2013-05-23 12:25:18 -0500

    biogeographic network functions

--- R/taxon_network.r
+++ R/taxon_network.r
@@ -1,12 +1,15 @@
 #' Create a bipartite graph of taxonomic occurence
 #'
 #'
-#'
-#'
-#'
+#' @param pbdb object of class pbdb
+#' @param weight logical indicating if resulting graph object is weighted
+#' @keywords
+#' @export
+#' @author Peter D Smits <psmits@uchicago.edu>
+#' @examples
 library(igraph)
 library(reshape2)
-taxon.bipartite <- function(pbdb, group = 'formation', weight = FALSE) {
+taxon.bipartite <- function(pbdb, weight = FALSE) {
   nn <- melt(cbind(rownames(tt$occurence), tt$occurence))
 
   if(!weight) {
